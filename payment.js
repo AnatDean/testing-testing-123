c2987a7fa58483afaecdc3293ab7c98420219c90
anat dean <anat.dean@hotmail.com>
2022-12-12 14:51:51 +0000

    update const

--- payment.js
+++ payment.js
@@ -2,7 +2,7 @@ const { divide } = require("./operations/named.exports.index");
 const calculate = require("./calculator");
 
 const makePayment = (price, fundsInPennies) => {
-  const fundsInPounds = calculate(fundsInPennies, divide, 100);
+  let fundsInPounds = calculate(fundsInPennies, divide, 100);
   fundsInPounds -= price;
   return { success: 200 };
 };
